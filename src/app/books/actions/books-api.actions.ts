303ab905bc91dac81b52a9cca41e024d92457545
Ardit Daku <arditdaku1@gmail.com>
2021-07-10 17:15:50 +0200

    Wip

--- src/app/books/actions/books-api.actions.ts
+++ src/app/books/actions/books-api.actions.ts
@@ -1,2 +1,26 @@
 import { createAction, props } from "@ngrx/store";
-import { BookModel } from "src/app/shared/models";
+import { BookModel, BookRequiredProps } from "src/app/shared/models";
+
+export const booksLoaded = createAction(
+  "[Books Api] Books Loaded Success",
+  props<{
+    books: BookModel[];
+  }>()
+);
+
+export const bookCreated = createAction(
+  "[Books Api] Create Book",
+  props<{
+    book: BookModel;
+  }>()
+);
+
+export const bookUpdated = createAction(
+  "[Books Api] Update Book",
+  props<{ book: BookModel }>()
+);
+
+export const bookDeleted = createAction(
+  "[Books Api] Delete Book",
+  props<{ bookId: string }>()
+);
--- src/app/books/actions/books-page.actions.ts
+++ src/app/books/actions/books-page.actions.ts
@@ -1,2 +1,28 @@
 import { createAction, props } from "@ngrx/store";
 import { BookRequiredProps } from "src/app/shared/models";
+
+export const enter = createAction("[Books Page] Enter");
+
+export const selectBook = createAction(
+  "[Book Page] Select Book",
+  props<{ bookId: string }>()
+);
+
+export const clearSelectedBook = createAction(
+  "[Book Page] Clear Selected Book"
+);
+
+export const createBook = createAction(
+  "[Books Page] Create a Book",
+  props<{ book: BookRequiredProps }>()
+);
+export const updateBook = createAction(
+  "[Books Page] Update a Book",
+  props<{ bookId: string; changes: BookRequiredProps }>()
+);
+export const deleteBook = createAction(
+  "[Books Page] Update a Book",
+  props<{ bookId: string }>()
+);
+
+export const cancelEditing = createAction("[Books Page] Cancel Editing a Book");
